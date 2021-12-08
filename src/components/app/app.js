11af398cb8a18ce56b34d7431305c5f7b22157dc
darthMilash <darthmilash@gmail.com>
2021-12-08 16:56:29 +0200

    Install React Js. Add accPost components/container

---
+++ src/components/app/app.js
@@ -0,0 +1,20 @@
+import { AccPostContainer } from "../../containers/accPost/accPost.js";
+import './app.css';
+
+
+function App() {
+    return (
+        <div className="app">
+            <header className="App-header">
+                <AccPostContainer author="Elon Musk" 
+                handle="elonmusk" 
+                verified='&#10003;' 
+                date='08.12.2021' 
+                content='As always, Tesla is looking for hardcore AI engineers who care about solving problems that directly affect people’s lives in a major way.' />
+            </header>
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/containers/accPost/accPost.js
@@ -0,0 +1,6 @@
+import {AccPost} from "../../components/accPost/accPost.js";
+
+export function AccPostContainer({ author, handle, verified, content, date}) {
+    const authorPost = `${author} @${handle}`;
+    return <AccPost authorPost={authorPost}  verified={verified} content={content} date={date}/>
+}
