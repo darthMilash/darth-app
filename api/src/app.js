8214ea3d05941e92f80152098d7c7683ed0da880
darthMilash <darthmilash@gmail.com>
2021-12-12 19:33:38 +0200

    add husky. add lint-staged. fixes .prettierrs.json (convert to UTF-8)

--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,15 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3001;
-const domain = process.env.DOMAIN;
- 
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3001
+const domain = process.env.DOMAIN
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
-});
- 
+    res.send('Hello World')
+})
+
 app.listen(port, () => {
-    console.log('server started on ' + domain + ':' + port);
-});
+    console.log(`server started on ${domain}:${port}`)
+})
