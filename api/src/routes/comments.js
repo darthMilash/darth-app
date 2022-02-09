b21cb66af762f20fa63103cba4fe668f2c336dda
darthMilash <darthmilash@gmail.com>
2022-02-09 16:44:11 +0200

    Create comments service

--- api/src/routes/comments.js
+++ api/src/routes/comments.js
@@ -1,51 +1,61 @@
 const router = require('express').Router()
 const commentsService = require('../services/store/comments.service')
-const db = require('../services/db')
 
 router.get('/', async (req, res) => {
-    res.send(await commentsService.getAllComments())
+    try {
+        res.status(200).json(await commentsService.getAllComments())
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 router.get('/:id', async (req, res) => {
-    const commentId = req.params.id
-    res.send(await commentsService.getCommentId(commentId))
+    try {
+        const commentId = req.params.id
+        res.status(200).json(await commentsService.getCommentId(commentId))
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 router.post('/', async (req, res) => {
-    const { userprofileid, postid, content, maincomment } = req.body
-    const date = new Date().toLocaleString()
-    await db
-        .insert({
-            userprofileid,
-            postid,
-            content,
-            datecreate: date,
-            maincomment,
-        })
-        .into('userpostcomments')
-    res.send(`The comment was created`)
+    try {
+        const { userprofileid, postid, content, maincomment } = req.body
+        const date = new Date().toLocaleString()
+        res.status(200).json(
+            await commentsService.postComment({
+                userprofileid,
+                postid,
+                content,
+                date,
+                maincomment,
+            })
+        )
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 router.put('/:id', async (req, res) => {
-    const { content } = req.body
-    const commentId = req.params.id
-    const dateUpdate = new Date().toLocaleString()
-    await db('userpostcomments')
-        .update({
-            content,
-            dateupdate: dateUpdate,
-        })
-
-        .where('userpostcommentsid', '=', commentId)
-    res.send(`The post "${commentId}" updated succesful"`)
+    try {
+        const { content } = req.body
+        const commentId = req.params.id
+        const dateUpdate = new Date().toLocaleString()
+        res.status(200).json(
+            await commentsService.putComment({ commentId, content, dateUpdate })
+        )
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 router.delete('/:id', async (req, res) => {
-    const commentId = req.params.id
-    await db('userpostcomments')
-        .delete()
-        .where('userpostcommentsid', '=', commentId)
-    res.send(`The post "${commentId}" deleted succesful`)
+    try {
+        const commentId = req.params.id
+        res.status(200).json(await commentsService.delCommentId(commentId))
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 module.exports = router
--- api/src/services/store/comments.service.js
+++ api/src/services/store/comments.service.js
@@ -5,4 +5,29 @@ module.exports = {
         db.select().from('userpostcomments').orderBy('userpostcommentsid'),
     getCommentId: async (id) =>
         db.select().from('userpostcomments').where('userpostcommentsid', id),
+    postComment: async ({
+        userprofileid,
+        postid,
+        content,
+        date,
+        maincomment,
+    }) =>
+        db
+            .insert({
+                userprofileid,
+                postid,
+                content,
+                datecreate: date,
+                maincomment,
+            })
+            .into('userpostcomments'),
+    putComment: async ({ commentId, content, date }) =>
+        db('userpostcomments')
+            .update({
+                content,
+                dateupdate: date,
+            })
+            .where('userpostcommentsid', commentId),
+    delCommentId: async ({ commentId }) =>
+        db('userpostcomments').delete().where('userpostcommentsid', commentId),
 }
